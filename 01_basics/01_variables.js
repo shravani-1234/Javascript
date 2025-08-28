70d8c47b5abc6ac0d1fee09b1b39ee99d9a0791c
shravani-1234 <120002464+shravani-1234@users.noreply.github.com>
2025-08-28 12:10:27 +0000

    data_type and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+
+const accountid= 123334
+let accountemail="shravani@gmail.com"
+var accountpassword="qwqwwe"
+
+accountCITY="telangan"
+// accountid=2
+accountemail="shravs@gmail.com"
+accountpassword="weewudedkh"
+accountCITY="hyderabad"
+ let accountstate;
+console.log(accountid)
+console.table([accountemail,accountid,accountpassword,accountCITY,accountstate])
+
+/*prefer not to use var becuase of issue in block scope and functional scope*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+
+"use strict" //use all js code as newer version
+
+// alert("hello")
+
+let name="shravani"
+let age="20"
+let isLoggedIn=false
+
+//number=>2 power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null is standalone value
+//undefined
+//symbol=>unique
+
+//object
+//null is an object
+
+
+console.log(typeof "shravani")
+console.log(typeof undefined)
+console.log(typeof null )
