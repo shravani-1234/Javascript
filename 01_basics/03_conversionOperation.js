5c378a0691837d5acc9c40905e557ae548e905c1
shravani-1234 <120002464+shravani-1234@users.noreply.github.com>
2025-08-28 12:46:49 +0000

    Conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+
+// let score="33abc"
+// let score=null
+
+// let score=true
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 false=>0
+
+let score="shravani"
+
+// let isLoggedIn=1
+let isLoggedIn=""
+let booleanisloogedIn=Boolean(isLoggedIn)
+//1=>true 0=>false ""=>false value string=>true
+
+console.log( booleanisloogedIn)
+
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+// const {score}=removeEventListener.body
+
+
+let num=12
+
+let stringnumber=String(num)
+
+console.log( typeof stringnumber)
